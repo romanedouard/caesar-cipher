b94c4ae4cd178ed7ecae2ba4729917dc4877efb5
Roman Edouard <123408909+romanedouard@users.noreply.github.com>
2024-05-11 17:28:34 -0400

    Get the secret message, alphabet characters, and shift key value. Create an empty caesar_cipher function.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,2 +1,15 @@
 # frozen_string_literal: true
 
+alphabet_array = ('a'..'z').to_a * 1000
+
+puts 'Enter your secret message:'
+secret_message = gets.chomp
+
+puts 'Enter the key:'
+key = gets.chomp.to_i
+
+def caesar_cipher(msg, shift)
+  
+end
+
+caesar_cipher(secret_message, key)
