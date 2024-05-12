b395511db826deef2eb2952793376926c27e189b
Roman Edouard <123408909+romanedouard@users.noreply.github.com>
2024-05-11 20:29:22 -0400

    Add logic to the caesar_cipher method to generate the encrypted text. Display the encrypted message to the user in the console.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -8,8 +8,18 @@ secret_message = gets.chomp
 puts 'Enter the key:'
 key = gets.chomp.to_i
 
-def caesar_cipher(msg, shift)
-  
+def caesar_cipher(msg, shift, alphabet)
+  encrypted_message = []
+  msg.each_char do |letter|
+    if alphabet.include?(letter)
+      alpha_letter_index = alphabet.find_index(letter)
+      encrypted_message.push(alphabet[alpha_letter_index + shift])
+    else
+      encrypted_message.push(letter)
+    end
+  end
+  encrypted_message.join('')
 end
 
-caesar_cipher(secret_message, key)
+encrypted_text = caesar_cipher(secret_message, key, alphabet_array)
+puts "Your secret message is #{encrypted_text}"
